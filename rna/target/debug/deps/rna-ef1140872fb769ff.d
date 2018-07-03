e8eb49d80c7b91edda6c37abca064662f2e69efb
SherMM <helioblast86@gmail.com>
2018-07-03 19:58:22 -0400

    finished rna problem

---
+++ rna/target/debug/deps/rna-ef1140872fb769ff.d
@@ -0,0 +1,5 @@
+/Users/quantumchuck/Projects/rust-rosalind/rna/target/debug/deps/rna-ef1140872fb769ff: src/main.rs
+
+/Users/quantumchuck/Projects/rust-rosalind/rna/target/debug/deps/rna-ef1140872fb769ff.d: src/main.rs
+
+src/main.rs:
---
+++ rna/target/debug/rna.d
@@ -0,0 +1,1 @@
+/Users/quantumchuck/Projects/rust-rosalind/rna/target/debug/rna.dSYM: /Users/quantumchuck/Projects/rust-rosalind/rna/src/main.rs
