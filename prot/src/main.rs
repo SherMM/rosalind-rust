106b9eb7794af1b18543c6149ac28536842f6bf8
Shermm <helioblast86@gmail.com>
2018-12-02 14:39:50 -0500

    finished protein problem

--- prot/src/main.rs
+++ prot/src/main.rs
@@ -1,10 +1,17 @@
 use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn translate_rna_to_protein(rna: String, codons: HashMap<&str, &str>) -> String {
-    let protein = String::new();
-    for nucleotide in rna.chars() {
+    let mut protein = String::new();
+    for i in (0..rna.len()-2).step_by(3) {
+    	let codon = &rna[i..i+3];
+    	let acid = codons.get(codon).unwrap();
+    	if acid == &"Stop" {break;}
+    	else {protein.push_str(acid);}
     }
-    protein;
+    protein
 }
 
 
@@ -28,7 +35,13 @@ fn main() {
         ("UGG", "W"),    ("CGG", "R"), ("AGG", "R"), ("GGG", "G") 
     ].iter().cloned().collect();
 
-    for (codon, acid) in &codons {
-        println!("{} {}", codon, acid);
-    }
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut rna = String::new();
+    f.read_to_string(&mut rna)
+        .expect("unable to read file");
+
+    println!("{}", translate_rna_to_protein(rna, codons));
 }
